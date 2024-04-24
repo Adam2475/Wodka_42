7adec075e5cb11fd996b3589e50b1c946a9603c9
Adam Natan Passaretti <adapassa@c2r4p12.42firenze.it>
2024-04-24 18:50:01 +0200

    done_scaffolding

---
+++ components/header/index.js
@@ -0,0 +1,28 @@
+import Layout from '../layout';
+import React from "react";
+
+export default function Header () {
+  return (
+    <div>
+      <div id="main-navbar" className="navbar">
+        <h2 className="logo">Sand</h2>
+        <nav>
+          <ul>
+            <li>
+              <a href="/home">Home</a>
+            </li>
+            <li>
+              <a href="/home">Home</a>
+            </li>
+            <li>
+              <a href="/home">Home</a>
+            </li>
+            <li>
+              <a href="/home">Home</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
 import "@/styles/globals.css";
+import Layout from "../components/layout/index.js"
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+	<Layout>
+		<Component {...pageProps} />
+	</Layout>
+  );
 }
