4a972fd51ae9290754294c2a4c59bdd496f3643a
Adam Natan Passaretti <adapassa@c2r5p11.42firenze.it>
2024-05-07 15:48:59 +0200

    adding_banner

--- components/layout/index.js
+++ components/layout/index.js
@@ -1,11 +1,13 @@
 import Header from '../header'
 import Footer from '../footer'
 import Card from '../cards/card.js'
+import Banner from '../banners/banner.js'
  
 export default function Layout({ children }) {
   return (
     <container>
       <Header />
+      <Banner/>
       <main>{children}</main>
       <Footer />
     </container>
